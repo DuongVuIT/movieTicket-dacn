027e37470ea5dee3f26a41692f23c329512efcd6
DuongVuIT <duonghutech26@gmail.com>
2023-09-21 17:16:07 +0700

    commit

--- src/api/firebase.ts
+++ src/api/firebase.ts
@@ -1,18 +1,18 @@
-import {getDatabase} from 'firebase/database';
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
+// import {getDatabase} from 'firebase/database';
+// import firebase from 'firebase/compat/app';
+// import 'firebase/compat/auth';
+// import 'firebase/compat/firestore';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyB89X3sWgSJCNnSU-o-bo-KiXUYUaZJO8k',
-  authDomain: 'movieticket-befc8.firebaseapp.com',
-  databaseURL: 'https://movieticket-befc8-default-rtdb.firebaseio.com',
-  projectId: 'movieticket-befc8',
-  storageBucket: 'movieticket-befc8.appspot.com',
-  messagingSenderId: '10166869724',
-  appId: '1:10166869724:web:06270daae182fb4e221e19',
-};
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyB89X3sWgSJCNnSU-o-bo-KiXUYUaZJO8k',
+//   authDomain: 'movieticket-befc8.firebaseapp.com',
+//   databaseURL: 'https://movieticket-befc8-default-rtdb.firebaseio.com',
+//   projectId: 'movieticket-befc8',
+//   storageBucket: 'movieticket-befc8.appspot.com',
+//   messagingSenderId: '10166869724',
+//   appId: '1:10166869724:web:06270daae182fb4e221e19',
+// };
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-export default firebase;
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+// export default firebase;
