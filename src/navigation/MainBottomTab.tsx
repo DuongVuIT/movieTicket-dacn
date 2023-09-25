7019cc1477164a7ff8733bc820650bd136e1025b
DuongVuIT <duonghutech26@gmail.com>
2023-09-25 11:50:37 +0700

    animation  bottom tab

--- src/navigation/MainBottomTab.tsx
+++ src/navigation/MainBottomTab.tsx
@@ -1,19 +1,50 @@
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import React from 'react';
-import {
-  BottomTabBarProps,
-  createBottomTabNavigator,
-} from '@react-navigation/bottom-tabs';
 
 import Account from '@screens/movie/Account';
 import Home from '@screens/movie/Home';
 import Search from '@screens/movie/Search';
 import Ticket from '@screens/movie/Ticket';
 import {RootParamList} from '@types';
-
+import LottieView from 'lottie-react-native';
 const MainBottomTab = () => {
   const Tab = createBottomTabNavigator<RootParamList>();
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color, size}) => {
+          let filePath;
+          let iconName;
+          switch (route.name) {
+            case 'Home':
+              filePath = require('@assets/lotties/home.json');
+
+              break;
+            case 'Search':
+              filePath = require('@assets/lotties/search.json');
+
+              break;
+            case 'Ticket':
+              filePath = require('@assets/lotties/ticket.json');
+
+              break;
+            case 'Account':
+              filePath = require('@assets/lotties/profile.json');
+              break;
+            default:
+              iconName = focused
+                ? 'ios-information-circle'
+                : 'ios-information-circle-outline';
+          }
+          return (
+            <LottieView
+              style={{height: '100%', width: '100%'}}
+              source={filePath}
+              autoPlay={focused}
+            />
+          );
+        },
+      })}>
       <Tab.Screen name="Home" component={Home} />
       <Tab.Screen name="Search" component={Search} />
       <Tab.Screen name="Ticket" component={Ticket} />
--- src/screens/movie/Account.tsx
+++ src/screens/movie/Account.tsx
@@ -1,10 +1,10 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-
+import LottieView from 'lottie-react-native';
 export default function Account() {
   return (
     <View>
-      <Text>Account</Text>
+      <Text>This is a Games Screen</Text>
     </View>
   );
 }
